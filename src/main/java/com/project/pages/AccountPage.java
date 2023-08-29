e55d69d68452d575e4245af0681c4af029b89cd3
MurariKumarThakur <murariraj.one@gmail.com>
2023-08-29 10:37:49 +0530

    base template

---
+++ src/main/java/com/project/pages/AccountPage.java
@@ -0,0 +1,35 @@
+package com.project.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.project.utils.CommonUtils;
+import com.project.utils.ElementUtils;
+
+
+
+public class AccountPage {
+	
+	WebDriver driver;
+	private ElementUtils elementUtils;
+	
+	public AccountPage(WebDriver driver) {
+		
+		this.driver = driver;
+		PageFactory.initElements(driver,this);
+		elementUtils = new ElementUtils(driver);
+		
+	}
+	
+	@FindBy(linkText="Edit your account information")
+	private WebElement editYourAccountInformationOption;
+	
+	public boolean displayStatusOfEditYourAccountInformationOption() {
+		
+		return elementUtils.displayStatusOfElement(editYourAccountInformationOption,CommonUtils.EXPLICIT_WAIT_BASIC_TIME);
+		
+	}
+
+}
---
+++ src/main/java/com/project/pages/AccountSuccessPage.java
@@ -0,0 +1,35 @@
+package com.project.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.project.utils.CommonUtils;
+import com.project.utils.ElementUtils;
+
+
+
+public class AccountSuccessPage {
+	
+	WebDriver driver;
+	private ElementUtils elementUtils;
+	
+	public AccountSuccessPage(WebDriver driver) {
+		
+		this.driver = driver;
+		PageFactory.initElements(driver,this);
+		elementUtils = new ElementUtils(driver);
+		
+	}
+	
+	@FindBy(xpath="//div[@id='content']/h1")
+	private WebElement pageHeading;
+	
+	public String getPageHeading() {
+		
+		return elementUtils.getTextFromElement(pageHeading,CommonUtils.EXPLICIT_WAIT_BASIC_TIME);
+		
+	}
+
+}
